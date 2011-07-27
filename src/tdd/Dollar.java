7bd080ec57d3d6de5f2b10caa565b89f0c51f961
shimomura ryosuke <r-shimomura@esm.co.jp>
2011-07-27 13:20:59 +0900

    tdd chapter '1' '2'

---
+++ src/tdd/Dollar.java
@@ -0,0 +1,15 @@
+package tdd;
+
+public class Dollar {
+
+	public int amount;
+
+	public Dollar(int amount)  {
+		this.amount=amount;
+	}
+
+	public Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+
+}
---
+++ src/tdd/DollarTest.java
@@ -0,0 +1,14 @@
+package tdd;
+
+import junit.framework.TestCase;
+
+public class DollarTest extends TestCase {
+	public void testMultiplication(){
+		Dollar five=new Dollar(5);
+		Dollar product=five.times(2);
+		assertEquals(10,product.amount);
+		product = five.times(3);
+		assertEquals(15,product.amount);
+	}
+
+}
