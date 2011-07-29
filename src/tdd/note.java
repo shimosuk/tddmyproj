37c0800b90fb2b29ecdf46dc51fe8aa63a0f629f
shimomura ryosuke <r-shimomura@esm.co.jp>
2011-07-29 13:32:21 +0900

    This note is written about how to using tgit

--- src/tdd/note.java
+++ src/tdd/note.java
@@ -14,7 +14,12 @@ public class note {
 	//Tortoisegit設定(環境変数)
 	/**
 	 * "マイ コンピュータ" -> "プロパティ" -> "環境変数"
-	 * ".gitconfig"をHOMEに置く
+	 * ".gitconfig"をHOMEに置く。またはHOMEを作成
+	 * 
+	 * <コマンド プロンプト操作>
+	 * パスを確認　echo "%HOME%"
+	 * 中身を確認 dir "%HOME%"
+	 * 
 	 * 
 	 */
 }
