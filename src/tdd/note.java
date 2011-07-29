b43cdf71d1e886258bd66c6b1fdab0a26e912653
unknown <shimosuk@.(none)>
2011-07-29 12:28:45 +0900

    -This note is written about how to using tgit

---
+++ src/tdd/note.java
@@ -0,0 +1,20 @@
+package tdd;
+
+public class note {
+	//操作
+	/**
+	 * tgit add .
+	 * 
+	 * tgit status
+	 * 
+	 * tgit commit -m "Comments" ※if you want to override,you write
+	 * "...commit --amound -m..."
+	 */
+	
+	//Tortoisegit設定(環境変数)
+	/**
+	 * "マイ コンピュータ" -> "プロパティ" -> "環境変数"
+	 * ".gitconfig"をHOMEに置く
+	 * 
+	 */
+}
