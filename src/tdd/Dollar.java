e29ecf53807d863219f24e841ba6579206e0c100
shimomura ryosuke <r-shimomura@esm.co.jp>
2011-08-04 12:02:23 +0900

    chapter6-2. 'equals:Dollar -> Money'

--- src/tdd/Dollar.java
+++ src/tdd/Dollar.java
@@ -11,10 +11,5 @@ public class Dollar extends Money{
 	public Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
-	
-	public boolean equals(Object object){
-		Dollar dollar = (Dollar) object;
-		return amount == dollar.amount;
-	}
 
 }
