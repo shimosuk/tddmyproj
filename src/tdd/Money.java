f9c02c6b61e0e34ea4bec4072c4b40cfd77fad8d
shimomura ryosuke <r-shimomura@esm.co.jp>
2011-08-10 12:47:34 +0900

    chaptor 8 finish

--- src/tdd/Money.java
+++ src/tdd/Money.java
@@ -1,10 +1,21 @@
 package tdd;
 
-public class Money {
-	protected int amount;
+public abstract class Money {
+	abstract Money times(int multiplier);
 
+	protected int amount;
+	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());
 	}
+	
+	static Money dollar(int amount){
+		return new Dollar(amount);
+	}
+	
+	static Money franc(int amount){
+		return new Franc(amount);
+	}
+
 }
